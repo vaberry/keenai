8bcd4c6b4fb5c03433e6d12044a328ec04ca46f8
vaberry <vincent.berry11@gmail.com>
2023-08-18 17:32:45 -0400

    e

--- base/urls.py
+++ base/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
    path('', views.Home.as_view(), name='home'),
    path('projects/', views.Projects.as_view(), name='projects'),
    path('about/', views.About.as_view(), name='about'),
-   path('newsletter/', views.Newsletter.as_view(), name='newsletter'),
+   path('newsletters/<str:industry>/', views.Newsletters.as_view(), name='newsletters'),
    path('articles/', views.Articles.as_view(), name='articles'),
 ]
--- base/views.py
+++ base/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.views.generic.base import View
+import requests
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
 class Home(View):
     def get(self, request, *args, **kwargs):
@@ -16,10 +20,40 @@ class About(View):
         if request.method == "GET":
             return render(request, 'about.html')
         
-class Newsletter(View):
+class Newsletters(View):
     def get(self, request, *args, **kwargs):
         if request.method == "GET":
-            return render(request, 'newsletter.html')
+            industry_subscribe_dict ={
+                'defense': {
+                    'link': 'https://embeds.beehiiv.com/eefc8670-7f04-4285-9250-5230991622cc?slim=true',
+                    'image': 'base/img/defenseai.png'
+                }
+            }
+            industry = kwargs.get('industry')
+            industry_dict = industry_subscribe_dict.get(industry)
+            print(industry_dict)
+            context = {
+                'industry_dict': industry_dict
+            }
+
+            # beehiiv_api_key = os.getenv("BEEHIIV_API_KEY")
+            # url = "https://api.beehiiv.com/v2/publications"
+            # headers = {
+            #     "Accept": "application/json",
+            #     "Authorization": f"Bearer {beehiiv_api_key}"
+            # }
+
+            # response = requests.get(url, headers=headers)
+
+            # if response.status_code == 200:
+            #     data = response.json()
+            #     # Process the JSON data here
+            #     print(data)
+            # else:
+            #     print("Request failed with status code:", response.status_code)
+
+
+            return render(request, 'newsletters.html', context=context)
         
 class Articles(View):
     def get(self, request, *args, **kwargs):
