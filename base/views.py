297f9f286cc37d1661f259300f46ab08ad1521ad
Vince Berry <vincent.berry11@gmail.com>
2024-03-22 15:00:05 -0500

    updated views

--- base/views.py
+++ base/views.py
@@ -18,29 +18,32 @@ class Home(View):
 class Projects(View):
     def get(self, request, *args, **kwargs):
         if request.method == "GET":
-            return render(request, 'projects.html')
+            # return render(request, 'projects.html')
+            return redirect('home')
         
 class About(View):
     def get(self, request, *args, **kwargs):
         if request.method == "GET":
-            return render(request, 'about.html')
+            # return render(request, 'about.html')
+            return redirect('home')
         
 class Newsletters(View):
     def get(self, request, *args, **kwargs):
         if request.method == "GET":
-            industry_subscribe_dict ={
-                'defense': {
-                    'link': 'https://embeds.beehiiv.com/eefc8670-7f04-4285-9250-5230991622cc?slim=true',
-                    'image': 'base/img/defenseai.png'
-                }
-            }
-            industry = kwargs.get('industry')
-            industry_dict = industry_subscribe_dict.get(industry)
+            # industry_subscribe_dict ={
+            #     'defense': {
+            #         'link': 'https://embeds.beehiiv.com/eefc8670-7f04-4285-9250-5230991622cc?slim=true',
+            #         'image': 'base/img/defenseai.png'
+            #     }
+            # }
+            # industry = kwargs.get('industry')
+            # industry_dict = industry_subscribe_dict.get(industry)
 
 
-            context = {
-                'industry_dict': industry_dict,
-                'newsletters': models.Newsletters.objects.all()
-            }
+            # context = {
+            #     'industry_dict': industry_dict,
+            #     'newsletters': models.Newsletters.objects.all()
+            # }
 
-            return render(request, 'newsletters.html', context=context)
+            # return render(request, 'newsletters.html', context=context)
+            return redirect('home')
